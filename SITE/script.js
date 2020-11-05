9e83e5781c5c2f23dfe8034202af6b0c7ec4a561
Samuel Formez <samuelformez@Carmen.local>
2020-11-05 16:26:15 +0100

    initial commit

---
+++ SITE/script.js
@@ -0,0 +1,62 @@
+var button1 = document.getElementById('button-semaine1');
+var button2 = document.getElementById('button-semaine2');
+var button3 = document.getElementById('button-semaine3');
+var button4 = document.getElementById('button-semaine4');
+var button5 = document.getElementById('button-semaine5');
+
+button1.addEventListener('click',AccessSemaine1);
+button2.addEventListener('click',AccessSemaine2);
+button3.addEventListener('click',AccessSemaine3);
+button4.addEventListener('click',AccessSemaine4);
+button5.addEventListener('click',AccessSemaine5);
+
+function AccessSemaine1(){
+    let mdpsemaine1 = document.getElementById('mdp-semaine1').value;
+    console.log(mdpsemaine1);
+    if (mdpsemaine1 == "coucou"){
+        window.location.href = "./conte-semaine-1.html";
+    } else{
+        alert("tu as entré un mauvais mot de passe ! Réesssaie! ")
+    }
+};
+
+function AccessSemaine2(){
+    let mdpsemaine2 = document.getElementById('mdp-semaine2').value;
+    console.log(mdpsemaine2);
+    if (mdpsemaine2 == "coucou"){
+        window.location = "http://www.youtube.com"
+    } else{
+        alert("tu as entré un mauvais mot de passe ! Réesssaie! ")
+    }
+};
+
+function AccessSemaine3(){
+    let mdpsemaine3 = document.getElementById('mdp-semaine3').value;
+    console.log(mdpsemaine3);
+    if (mdpsemaine3 == "coucou"){
+        window.location = "http://www.youtube.com"
+    } else{
+        alert("tu as entré un mauvais mot de passe ! Réesssaie! ")
+    }
+};
+
+function AccessSemaine4(){
+    let mdpsemaine4 = document.getElementById('mdp-semaine4').value;
+    console.log(mdpsemaine4);
+    if (mdpsemaine4 == "coucou"){
+        window.location = "http://www.youtube.com"
+    } else{
+        alert("tu as entré un mauvais mot de passe ! Réesssaie! ")
+    }
+};
+
+function AccessSemaine5(){
+    let mdpsemaine5 = document.getElementById('mdp-semaine5').value;
+    console.log(mdpsemaine5);
+    if (mdpsemaine5 == "coucou"){
+        window.location = "http://www.youtube.com"
+    } else{
+        alert("tu as entré un mauvais mot de passe ! Réesssaie! ")
+    }
+};
+
